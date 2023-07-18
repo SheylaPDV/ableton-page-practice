b57f4003dcd8685520921671e87d9c27fbc3b3a9
Sheyla <sheylapdv@gmail.com>
2023-07-18 13:14:02 +0200

    init ableton page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <>
-      {' '}
-      <header className='header-app'>
-        <div className='header-items'>
-          <p>Live</p>
-          <p>Push</p>
-          <p>Link</p>
-          <p>Shop</p>
-          <p>Packs</p>
-          <p>Help</p>
-          <p>More +</p>
-        </div>
-        <div className='header-items2'>
-          <p>Try Live for free</p>
-          <a href='/'>
-            <p>Log in or register</p>
-          </a>
-        </div>
-      </header>
+      <Header />
       <div className='app'>
-        {' '}
-        <div>Esto es la APP</div>
+        <div className='container-img'>
+          <img src='/src/assets/pexels-dsd-1822605.jpg' />
+        </div>
       </div>
     </>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header className='header-app'>
+      <div className='header-items'>
+        <p>Live</p>
+        <p>Push</p>
+        <p>Link</p>
+        <p>Shop</p>
+        <p>Packs</p>
+        <p>Help</p>
+        <p>More +</p>
+      </div>
+      <div className='header-items2'>
+        <p>Try Live for free</p>
+        <a href='/'>
+          <p>Log in or register</p>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
